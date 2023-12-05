03d1e3410ac1e41dbf216432375577e633396ab5
Alexandros Tselios <alexandros.tselios@peoplecert.onmicrosoft.com>
2023-12-05 10:54:27 +0200

    Alexandros commit

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -5,6 +5,6 @@ class Program
     {
         System.Console.WriteLine("This is the first paragraph");
         System.Console.WriteLine("This is the Second paragraph");
-        Console.WriteLine("This is the third paragraph");
+        Console.WriteLine("Hello From Alexandros");
     }
 }
