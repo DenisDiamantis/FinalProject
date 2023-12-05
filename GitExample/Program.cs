69431be8e8e84dadeb640fdeab77f1cbcb99d5af
Athanasios Anastasiou <Athanasios.Anastasiou@hotmail.com>
2023-12-05 10:50:02 +0200

    Hello from Thanos

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -4,5 +4,6 @@
     {
         System.Console.WriteLine("This is the first paragraph");
         System.Console.WriteLine("This is the Second paragraph");
+        Console.WriteLine("Hello from Thanos");
     }
 }
