239e92edb783ccb4ea35909f8c79c5a74e3b912c
Lazaros Sekkas <contact.lazaros@gmail.com>
2023-12-05 10:52:36 +0200

    first test lazaros

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -4,5 +4,6 @@
     {
         System.Console.WriteLine("This is the first paragraph");
         System.Console.WriteLine("This is the Second paragraph");
+        System.Console.WriteLine("Third paragraph with lazaros");
     }
 }
