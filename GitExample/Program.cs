b1a61fce1f0bc8ffa55b50058daeed1121b402da
Alexandros Tselios <alexandros.tselios@peoplecert.onmicrosoft.com>
2023-12-05 10:51:57 +0200

    Third Commit

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -1,8 +1,10 @@
-﻿class Program
+﻿using System;
+class Program
 {
     static void Main()
     {
         System.Console.WriteLine("This is the first paragraph");
         System.Console.WriteLine("This is the Second paragraph");
+        Console.WriteLine("This is the third paragraph");
     }
 }
